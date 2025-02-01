6d4f4237d98808c0a843c03c2a2fecf4daf8739b
andreabellmunt <andrea.bellmunt@aalto.fi>
2025-02-01 16:31:35 +0200

    Relational database + sqlmodel changes

--- main.py
+++ main.py
@@ -1,67 +1,68 @@
-from fastapi import FastAPI
-from mongoengine import connect, disconnect, Document, StringField, IntField
-from enum import Enum 
-from pydantic import BaseModel # offers data validation 
+from fastapi import FastAPI, Depends
+from decimal import Decimal 
+from sqlmodel import SQLModel, Field, create_engine, Session # uses pydantic + sqlalchemy: data validation + interaction with db
 
-app = FastAPI()
 
 """ 
-    DEFINING DATA CLASSES
+    DEFINING THE DATA MODELS
 """
-# DB attributes
-class Seashell(Document):
-    id = IntField(required = True)
-    name = StringField()
-    species = StringField()
-    color = StringField()
-    size = StringField()
-    addDescription = StringField()
+class SeashellBase(SQLModel): # Base data model to generate others
+    name: str | None 
+    species: str | None 
+    color: str | None
+    weight:  Decimal = Field(max_digits=5, decimal_places=3) 
+    addDescription: str | None 
 
-# Type for class SeashellData
-class SizeEnum(str, Enum):
-    S = 'S' #small
-    M = 'M' #medium
-    L = 'L' #large 
+class Seashell(SeashellBase, table = True): # DB model 
+    id: int | None = Field(primary_key = True) # DB generates id
+    
+class SeashellAdd(SeashellBase): # Request model (we do NOT ask for id)
+    pass # exactly the same as base
 
-# Data model managing input data (same structure as DB)
-class SeashellData(BaseModel):
-    id: int
-    name: str | None
-    species: str | None
-    color: str | None 
-    size: SizeEnum | None # Possible values: 'S', 'M', or 'L'
-    addDescription: str | None # Additional description 
+class SeashellResp(SeashellBase): # Response model 
+    id: int # for responses, sharing the id 
 
 
 """
     MANAGING THE DB CONNECTION: 
-    - Name of the DB: seashells-db
-    - Connect and disconnect based on app state 
+    - DB: SQLite (uses one file)
+    - Name of the file DB: seashells.db
+    - Need to have Session to interact with DB and track changes 
 """
 
-@app.on_event("startup")
-async def connect_DB():
-    connect("seashells-db") # default host and port are localhost 27017 
-    
-@app.on_event("shutdown")
-async def disconnect_DB(): 
-    disconnect("seashells-db")
+db_file = "seashells.db"
+db_url = f"sqlite:///{db_file}" # required url format 
+engine = create_engine(db_url)# object handling comm with DB 
+
+def generate_table(): 
+    """ 
+    Generates table in DB if NOT existing yet 
+    """
+    SQLModel.metadata.create_all(engine)
+
+def get_session(): 
+    """
+    Generates a new session dependency (each request uses a session)
+    """
+    with Session(engine) as session: 
+        yield session 
 
 """
     API FUNCTIONS 
 """
-@app.get("/seashells") 
-async def list_seashells():
+app = FastAPI()
+
+@app.on_event("startup")
+def start_DB():
+    """
+    Calls generate_table() function, which creates or 
+    ensures the table 'seashells' exists in the DB
+    """
+    generate_table()
+    
+@app.get("/seashells", response_model = list[SeashellResp]) 
+def list_seashells(session: Session = Depends(get_session)):
     return 
     
 @app.post("/seashells")
-async def add_seashell(seashells: SeashellData):
-    
-    
-        
-
-    
-
-    
-    
-
+async def add_seashell():
