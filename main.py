1af09d1bb6e1aef7a59f5198d9dc636a10bcdcf2
andreabellmunt <andrea.bellmunt@aalto.fi>
2025-02-01 17:49:24 +0200

    Main developed (pre-test)

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from decimal import Decimal 
-from sqlmodel import SQLModel, Field, create_engine, Session # uses pydantic + sqlalchemy: data validation + interaction with db
-
+from sqlmodel import SQLModel, Field, create_engine, Session, select # uses pydantic + sqlalchemy: data validation + interaction with db
 
 """ 
     DEFINING THE DATA MODELS
@@ -62,7 +61,55 @@ def start_DB():
     
 @app.get("/seashells", response_model = list[SeashellResp]) 
 def list_seashells(session: Session = Depends(get_session)):
-    return 
+    """ 
+    Description: Lists all the seashells found in the DB table. 
+    Parameters: Session dependency. 
+    Returns: list of SeashellResp objects (id is included). 
+    """
+    seashells = session.exec(select(Seashell)).all() # SELECT * FROM seashells
+    return seashells # Validation for list of SeashellResp object type
     
-@app.post("/seashells")
-async def add_seashell():
+@app.post("/seashells", response_model = SeashellResp)
+def add_seashell(new_seashell: SeashellAdd, session: Session = Depends(get_session)):
+    """ 
+    Description: Add a new Seashell object in the DB.  
+    Parameters: SeashellAdd object (does not contain id), Session dependency. 
+    Returns: SeashellResp object saved (contains id) 
+    """
+    seashell = Seashell.validate(new_seashell) 
+    session.add(seashell) # Add new object 
+    session.commit() # Commit changes in DB
+    session.refresh(seashell) # Expire (remove loaded data from mem) + refresh value of seashells 
+    return seashell # Validation for SeashellResp object type 
+    
+@app.patch("/seashells/{seashell_id}", response_model = SeashellResp)
+def edit_seashell(seashell_id: int, new_seashell: SeashellAdd, session: Session = Depends(get_session)): 
+    """ 
+    Description: Edits an existing Seashell object entry  in the DB
+    Parameters: ID for the seashell, SeashellAdd object (does not contain id), Session dependency. 
+    Returns: SeashellResp object updated (contains id) 
+    """
+    seashell = session.get(Seashell, seashell_id)
+    if not seashell: 
+        raise HTTPException(status_code = 404, detail = "Seashell not found")
+    # UPDATE object 
+    changes_seashell = new_seashell.model_dump(exclude_unset = True) # Only getting changed attributes
+    seashell.sqlmodel_update(changes_seashell) # Updating object 
+    session.add(seashell) # Add new object to DB 
+    session.commit()
+    session.refresh(seashell)
+    return seashell # Validation for SeashellResp object type
+
+@app.delete("/seashells/{seashell_id}", status_code = 204)
+def delete_seashell(seashell_id: int, session: Session = Depends(get_session)):
+    """ 
+    Description: Deletes an existing Seashell object entry  in the DB
+    Parameters: ID for the seashell, Session dependency. 
+    Return: Status code and, if successful, OK message
+    """
+    seashell = session.get(Seashell, seashell_id)
+    if not seashell: 
+        raise HTTPException(status_code = 404, detail = "Seashell not found")
+    session.delete(seashell) # Deleting object 
+    session.commit() # Commit changes
+    return {"Message" : f"Seashell {seashell_id} deleted"} # Successful message 
